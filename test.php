3c27f838ef5813b747896d6941dd627a28a5ba48
Anshul Pareek <anshul.pareek@ranosys.com>
2019-02-05 16:15:02 +0530

    added after checking checkout

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php 
+echo "testing git";
+echo "i am here";
+echo "checking git show";
+echo "showing diff command";
+echo "added for amend";
+echo "checking diff command";
+echo "check for git stash";
+echo "here we are checking checkout command";
+echo "after stash this line added";
+?>
