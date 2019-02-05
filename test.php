7a996ce02ce429bcae0f6e700914f0dc38ec566d
Anshul Pareek <anshul.pareek@ranosys.com>
2019-02-05 15:31:24 +0530

    commit after stash and resolving conflicts

--- test.php
+++ test.php
@@ -4,5 +4,7 @@ echo "i am here";
 echo "checking git show";
 echo "showing diff command";
 echo "added for amend";
+echo "checking diff command";
+echo "check for git stash";
 echo "after stash this line added";
 ?>
